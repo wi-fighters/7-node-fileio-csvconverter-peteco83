b115ae37174ce4724097560ebc85ba666ff7fa11
peteco83 <peteco1983@outlook.com>
2020-03-28 13:19:50 +0100

    add 1 till 3 missing if an error occured

---
+++ index.js
@@ -0,0 +1,33 @@
+const csv = require('csvtojson')
+const path = require('path')
+const fs = require('fs')
+const args = process.argv.slice(2)
+
+let [csvFilePath, toCsvFilePath] = process.argv.slice(2)
+
+const converter = (fromfile, tofile) => {
+
+    if (fromfile && tofile) {
+        csv()
+            .fromFile(path.resolve(__dirname, csvFilePath))
+            .then((jsonObj) => {
+                fs.writeFileSync(__dirname + `/${tofile}`, JSON.stringify(jsonObj))
+                console.log(`JSON file saved at: ${tofile}`)
+            })
+    } else if (fromfile && !tofile) {
+        csv()   
+        .fromFile(path.resolve(__dirname, csvFilePath))
+        .then((jsonObj) => {
+            fs.writeFileSync(__dirname + "/demo.json", JSON.stringify(jsonObj))
+            console.log(`JSON file saved at: ${fromfile}.json`)
+        })
+
+    } else if (!fromfile && !tofile) {
+        console.log("Please provide a csv file to convet to JSON")
+        process.exit
+    } 
+
+}
+
+
+converter(csvFilePath, toCsvFilePath)
